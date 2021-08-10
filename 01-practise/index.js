dd160920e6122fb059286fed7edf577aa2b328bf
AlexKasatov <48275092+AlexKasatov@users.noreply.github.com>
2021-08-10 23:21:34 +0300

    practise-01-done

---
+++ 01-practise/index.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = prompt("Write any HTML code!");
+
+// вызывает окно бразуера и просит ввести любой html code типа <h1>Hello</h1>,
+// callback от prompt переопределяется добавлением введеного кода в окно и добаляется в <body></body>
