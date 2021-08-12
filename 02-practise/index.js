0f8efcf8d1bba0657a5fe0a660544f8419e3ac72
AlexKasatov <48275092+AlexKasatov@users.noreply.github.com>
2021-08-12 16:41:38 +0300

    finish practise-2

---
+++ 02-practise/index.js
@@ -0,0 +1,19 @@
+let totalCash = prompt("Йо, Кевин, сколько кеша у тебя в наличии?");
+
+let watchesCount = prompt("Сколько часов ты взял?");
+
+let earringsCount = prompt("Ага, а сколько сережек с брилиантами?");
+
+let watchesCost = prompt("Сколько стоят часы?");
+
+let earringsCost = prompt("Сколько стоят сережки?");
+
+let earringsSum = parseInt(earringsCount) * parseInt(earringsCost);
+
+let watchesSum = parseInt(watchesCount) * parseInt(watchesCost);
+
+let totalPurchases = earringsSum + watchesSum;
+
+let totalBalance = totalCash >= totalPurchases;
+
+console.log(totalBalance);
